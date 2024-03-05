aefe7467b244ed6f589b1db912a99bf6cf674648
sreevalli-gude <sreevalli396@gmail.com>
2024-03-05 14:14:37 +0530

    sree

--- day3practice/areaofcircle.py
+++ day3practice/areaofcircle.py
@@ -2,5 +2,5 @@
 # the formula area=π×r^2 ( Take pie as 3.14)
 r = float(input("enter the radius of the circle"))
 pi = 3.143
-area = pi * r * r
-print("Area of a circle is",+area)
+area = pi * r **2
+print("Area of a circle is",area)
